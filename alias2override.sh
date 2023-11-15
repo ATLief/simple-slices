04707ca7203cf821e89bc33de9f996d01c14de67
Alex Lieflander <public@atlief.com>
2023-11-14 23:25:00 -0500

    Rename "system" preset to "server"

--- alias2override.sh
+++ alias2override.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for preset in system user; do
+for preset in server user; do
 	preset_def="ss_preset=${preset}"
 	for slice_alias in $(m4 -I inc -D "$preset_def" "$@" inc/extract-alias.m4); do
 		echo "$(basename "$0"): '$*' [${preset}] -> ${slice_alias} override"
