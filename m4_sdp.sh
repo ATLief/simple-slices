d4d2d1278fc856f225773ce2ae38229596972fce
ATLief <public@atlief.com>
2024-04-08 16:08:14 -0400

    Automatically define unit name as ss_name during preset generation

--- m4_sdp.sh
+++ m4_sdp.sh
@@ -3,9 +3,11 @@ set -e
 preset_arg="$1"
 child_name="$2"
 shift 2
+unit_name="$child_name"
 parent_path="build/systemd/${preset_arg}"
 if [ "$(echo "$child_name" | rev | cut -d . -f 1)" = "d" ]; then
 	override_num=20
+	unit_name="$(echo "$unit_name" | rev | cut -c 3- | rev)"
 	parent_path="${parent_path}/${child_name}"
 	for preset_iter in neutral user server desktop; do
 		if [ "$preset_iter" = "$preset_arg" ]; then
@@ -15,4 +17,4 @@ if [ "$(echo "$child_name" | rev | cut -d . -f 1)" = "d" ]; then
 	done
 fi
 mkdir -p "$parent_path"
-m4 -D "ss_preset=${preset_arg}" "$@" >"${parent_path}/${child_name}"
+m4 -D "ss_preset=${preset_arg}" -D "ss_name=${unit_name}" "$@" >"${parent_path}/${child_name}"
