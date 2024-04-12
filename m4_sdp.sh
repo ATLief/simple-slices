04b56ad5ad0461cb7e1dc8916b84211eeedb2f20
ATLief <public@atlief.com>
2024-04-11 20:55:12 -0400

    Revert "Move parent_path append outside preset iteration"
    This reverts commit 1497632105c11730b3459659033ad5e271b29228.

--- m4_sdp.sh
+++ m4_sdp.sh
@@ -8,9 +8,9 @@ parent_path="build/systemd/${preset_arg}"
 if [ "$(echo "$child_name" | rev | cut -d . -f 1)" = "d" ]; then
 	override_num=20
 	unit_name="$(echo "$unit_name" | rev | cut -c 3- | rev)"
-	parent_path="${parent_path}/${child_name}"
 	for preset_iter in neutral user server desktop; do
 		if [ "$preset_iter" = "$preset_arg" ]; then
+			parent_path="${parent_path}/${child_name}"
 			child_name="${override_num}-simple-slices.conf"
 		fi
 		override_num=`expr "$override_num" + 10`
