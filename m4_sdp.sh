8380100b31aacc1c66068412fe735ac015d85d56
ATLief <public@atlief.com>
2024-04-11 20:58:43 -0400

    Split override processing into 2 sections
    This always iterates presets, even when is_override is false

--- m4_sdp.sh
+++ m4_sdp.sh
@@ -5,17 +5,22 @@ child_name="$2"
 shift 2
 unit_name="$child_name"
 parent_path="build/systemd/${preset_arg}"
+override_num=20
 if [ "$(echo "$child_name" | rev | cut -d . -f 1)" = "d" ]; then
-	override_num=20
+	is_override=true
 	unit_name="$(echo "$unit_name" | rev | cut -c 3- | rev)"
-	for preset_iter in neutral user server desktop; do
-		if [ "$preset_iter" = "$preset_arg" ]; then
+else
+	is_override=false
+fi
+for preset_iter in neutral user server desktop; do
+	if [ "$preset_iter" = "$preset_arg" ]; then
+		if $is_override; then
 			parent_path="${parent_path}/${child_name}"
 			child_name="${override_num}-simple-slices.conf"
 		fi
-		override_num=`expr "$override_num" + 10`
-	done
-fi
+	fi
+	override_num=`expr "$override_num" + 10`
+done
 m4_args="-D ss_preset=${preset_arg} -D ss_name=${unit_name}"
 if m4 $m4_args "$@" >/dev/null; then
 	mkdir -p "$parent_path"
