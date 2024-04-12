f84c06bba9a626051167ead038bcca51e80c4295
ATLief <public@atlief.com>
2024-04-11 23:48:32 -0400

    Reset value of parent_path and child_name before each preset iteration

--- m4_sdp.sh
+++ m4_sdp.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 set -e
 preset_arg="$1"
-child_name="$2"
+child_name_orig="$2"
 shift 2
-unit_name="$child_name"
-parent_path="build/systemd/${preset_arg}"
+unit_name="$child_name_orig"
 override_num=20
-if [ "$(echo "$child_name" | rev | cut -d . -f 1)" = "d" ]; then
+if [ "$(echo "$child_name_orig" | rev | cut -d . -f 1)" = "d" ]; then
 	is_override=true
 	unit_name="$(echo "$unit_name" | rev | cut -c 3- | rev)"
 else
 	is_override=false
 fi
 for preset_iter in neutral user server desktop; do
+	parent_path="build/systemd/${preset_arg}"
+	if $is_override; then
+		parent_path="${parent_path}/${child_name_orig}"
+		child_name="${override_num}-simple-slices.conf"
+	else
+		child_name="$child_name_orig"
+	fi
 	m4_args="-D ss_preset=${preset_arg} -D ss_name=${unit_name}"
 	if [ "$preset_iter" = "$preset_arg" ]; then
-		if $is_override; then
-			parent_path="${parent_path}/${child_name}"
-			child_name="${override_num}-simple-slices.conf"
-		fi
 		if m4 $m4_args "$@" >/dev/null; then
 			mkdir -p "$parent_path"
 			m4 $m4_args "$@" >"${parent_path}/${child_name}"
